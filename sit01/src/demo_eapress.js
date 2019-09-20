52a2ea17cffa79cf1982205fec6d620ef7cf14e1
Winnie <winniexx.0918@gmail.com>
2019-09-20 17:37:31 +0800

    EJSTAGS，使用JSON資料檔，include

---
+++ sit01/src/demo_eapress.js
@@ -0,0 +1,14 @@
+const winnie = function(){
+    return {name:'shin'};
+};
+
+shin.my_func = function(){
+    return {name: 'my_func'};
+};
+
+console.log(winnie());
+console.log(winnie.my_func());
+
+
+
+
--- sit01/src/index.js
+++ sit01/src/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const url = require('url');
+const bodyParser = require('body-parser');
+
+const urlencodedParser = bodyParser.urlencoded({extended:false});
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 
 //靜態內容資料夾
@@ -11,11 +17,42 @@ app.set('view engine', 'ejs');
 
 //routes 路由
 app.get('/',(req,res)=>{
-    res.render('home',{name:'winnie'});
+    res.render('home',{name:'winnie',a:123});
 });
-app.get('/winnie',(req,res)=>{
-    res.send('hello winnie');
+//app.get('/',(req,res)連結檔跟目錄
+
+// app.get('/winnie',(req,res)=>{
+//     res.send('hello winnie');
+// });
+
+app.get('/sales01',(req,res)=>{
+    const data = require('./../data/sales01');
+     //res.send(JSON.stringify(sales));
+     res.render('sales01', {
+        sales:  data
+     });
 });
+
+
+app.get('/try-qs',(req,res)=>{
+    const urlParts = url.parse(req.url,true);
+    console.log(urlParts);
+
+    res.render('try-qs',{
+        query: urlParts.query
+    });
+})
+
+
+
+app.get('/try-post-form',(req,res)=>{
+    res.render('try-post-form');
+})
+app.post('/try-post-form',urlencodedParser,(req,res)=>{
+    res.render('try-post-form',req.body);
+
+    // res.send(JSON.stringify(req.body))
+})
 
 //自訂404頁面
 app.use((req,res)=>{
